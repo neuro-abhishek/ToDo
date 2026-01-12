19123efeddd5f7c7b75770a08958eafdaf0b610b
Its-Abhishek-2907 <abishekhdubey2907@gmail.com>
2026-01-12 12:56:25 +0530

    another change

--- obj/Debug/net10.0/ToDo.AssemblyInfo.cs
+++ obj/Debug/net10.0/ToDo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c62207e6ade1ea61f9158ddfb24e4270b2a32f53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bc6ef475fcd72e44ce67dbb4dd8defd460cce04")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
